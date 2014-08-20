d5d1ba560201566fa6557d544b045cc822dd595f
steve hiemstra <stevehiemstra@gmail.com>
2014-08-19 22:47:29 -0400

    add simple demo equation field

--- javascripts/calc.js
+++ javascripts/calc.js
@@ -3,11 +3,39 @@
 
 	var demo = document.getElementById('demo'),
 	inputs = demo.getElementsByTagName('input'),
-	A = inputs[0], B = inputs[1];
+	A = inputs[0], B = inputs[2],
+	equation = "a * a",
+	offClick = function(e) {
+		//click off a window
+		console.log(e);
+		if ("INPUT" === e.target.nodeName && e.target.parentNode.id === "equation" || e.target.id === "equation"){
+			//ignore
+		} else {
+			var eq = document.getElementById('equation');
+			equation = inputs[1].value;
+			eq.classList.add('hidden');
+			window.removeEventListener('click', offClick);
+		}
+
+	}
 
 	A.addEventListener('keyup', function(e){
 		var a = parseInt(A.value, 10);
-		B.value = isNaN(a) ? '' : a * a;
+		B.value = isNaN(a) ? '' : eval(equation);
+	});
+
+	B.addEventListener('click', function(e){
+		var eq = document.getElementById('equation');
+			eq.classList.remove('hidden');
+		setTimeout(function(){
+			window.addEventListener('click', offClick);
+		}, 0);
+	});
+
+	inputs[1].addEventListener('keyup', function(e){
+		if (e.keyCode === 13){
+			offClick(e);
+		}
 	});
 
 }());
