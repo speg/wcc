9bab92c4d577d84519a5d045d0827f2f6af73144
steve hiemstra <stevehiemstra@gmail.com>
2014-08-25 21:53:49 -0400

    added ability to add inputs
    also set E to the second input at start, as inputs is a LIVE
    HTMLCollection.
    
    Todo: wire up new inputs to the equation system

--- javascripts/calc.js
+++ javascripts/calc.js
@@ -1,16 +1,22 @@
 (function(){
 	'use strict';
 
+	if ("true" === window.localStorage.mixer) {
+		mixpanel.track = function(e){console.log(e)};
+	}
+
 	var demo = document.getElementById('demo'),
 	inputs = demo.getElementsByTagName('input'),
-	A = inputs[0], B = inputs[2],
+	addInput = document.getElementById('addInput'),
+	A = inputs[0], B = inputs[2], E = inputs[1],
 	equation = "a * a",
 	offClick = function(e) {
 		if ("INPUT" === e.target.nodeName && e.target.parentNode.id === "equation" || e.target.id === "equation"){
 			//ignore
 		} else {
 			var eq = document.getElementById('equation');
-			equation = inputs[1].value;
+			console.log(inputs);
+			equation = E.value;
 			eq.classList.add('hidden');
 			window.removeEventListener('click', offClick);
 		}
@@ -24,12 +30,19 @@
 	});
 
 	B.addEventListener('click', function(e){
-		mixpanel.track("output click");
+		mixpanel.track("opened equation");
 		var eq = document.getElementById('equation');
 			eq.classList.remove('hidden');
 		setTimeout(function(){
 			window.addEventListener('click', offClick);
 		}, 0);
+	});
+
+	addInput.addEventListener('click', function(e){
+		mixpanel.track("added new input")
+		var newNode = document.createElement('input');
+		newNode.type = "text";
+		A.parentNode.insertBefore(newNode, A.nextSibling);
 	});
 
 	inputs[1].addEventListener('keyup', function(e){
